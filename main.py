882e0321a7b45fb35825c1a66940d254b16056a3
Devasish Mishra <devasishmishra74@gmail.com>
2025-11-13 14:54:14 +0530

    runnign app only if it in main scope 4

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ def gallery():
     return render_template("gallery.html", reels = reels)
 
 if __name__ == "__main__":
-    app.run(debug=False, use_reloader=False)
+    app.run(debug=True, use_reloader=False)
